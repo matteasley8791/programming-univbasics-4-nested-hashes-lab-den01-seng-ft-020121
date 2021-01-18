84914590d29fbe1944e10ca2939846956a14ff44
Matthew Easley <matteasley8791@gmail.com>
2021-01-18 23:19:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] = []
+programmer_hash[:yukihiro_matsumoto] = [
+  ]
 yukihiro_matsumoto[:known_for] = []
 programmer_hash[:yukihiro_matsumoto] => {languages: "LISP", "C"}
 end
